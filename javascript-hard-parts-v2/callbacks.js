41e5554b71f3d0218d956b26f643f2e020e2f439
Dustin Tauer <dustin@mjg.in>
2024-02-27 15:22:20 -0600

    fixes #22

--- javascript-hard-parts-v2/callbacks.js
+++ javascript-hard-parts-v2/callbacks.js
@@ -387,7 +387,7 @@ function addTen(num){
 // Challenge 23
 function myFunc(array, callback) {
   for (let i = 0; i < array.length; i++) {
-    if (callback(array[i])) return 1;
+    if (callback(array[i])) return i;
   }
   return -1;
 }
